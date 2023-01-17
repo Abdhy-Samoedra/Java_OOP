fe86db053ea2d2231efb08080eb2d927a555c535
abdhy <abdhysamoedra030@gmail.com>
2023-01-17 09:51:54 +0700

    update

--- Lecturer/src/Session4/Student.java
+++ Lecturer/src/Session4/Student.java
@@ -2,11 +2,49 @@ package Session4;
 
 public class Student {
     //ini atribut
-    String id;
-    String name;
-    String className;
+    private String id;
+    private String name;
+    private String className;
     //tidak seperti di c, di java tidak perlu inisialisasi = 0
     private int score; // dikasih private agar bisa diakses hanya di kelas ini sendiri (Stundent)
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    int getScore() {
+//        karena dia get, maka pasti memngembalikan suatu value, yaitu score itu sendiri
+        return score;
+    }
+
+    void setScore(int score) {
+
+        // ini namanya preprocessing, kelebihan setter bisa melakukan preprocessing yaitu kita bisa melakukan something terlebih dahulu
+        if (score > 100 || score < 0) {
+            throw new IllegalArgumentException("Score harus 0 - 100");
+        }
+        this.score = score;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
 
     //ini method
     void study() {
@@ -31,19 +69,7 @@ public class Student {
         System.out.println("Score : " + score);
     }
 
-    int getScore() {
-//        karena dia get, maka pasti memngembalikan suatu value, yaitu score itu sendiri
-        return score;
-    }
 
-    void setScore(int score) {
-
-        // ini namanya preprocessing, kelebihan setter bisa melakukan preprocessing yaitu kita bisa melakukan something terlebih dahulu
-        if (score > 100 || score < 0){
-            throw new IllegalArgumentException("Score harus 0 - 100");
-        }
-        this.score = score;
-    }
 
 
     /*"access modifier" biasa digunakan pada encapsulation(melindungi/memprotect)
