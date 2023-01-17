d314d20946591b6bcd66ec9b04ec4de0830922e8
abdhy <abdhysamoedra030@gmail.com>
2023-01-17 09:46:22 +0700

    update

---
+++ Lecturer/src/Session4/Student.java
@@ -0,0 +1,74 @@
+package Session4;
+
+public class Student {
+    //ini atribut
+    String id;
+    String name;
+    String className;
+    //tidak seperti di c, di java tidak perlu inisialisasi = 0
+    private int score; // dikasih private agar bisa diakses hanya di kelas ini sendiri (Stundent)
+
+    //ini method
+    void study() {
+        if (score + 10 < 100) {
+            score += 10;
+        } else {
+            score = 100;
+        }
+        System.out.println("Study" + score);
+    }
+
+    Student(String name, String className) {
+        id = "" + (int) (Math.random() * 2000000000); // cara ngrandom angka antara 0-2m, dikasih stirng "" kosong aar nanti menjadi string(lifehack), lalu di casting  ke int
+        this.name = name;
+        this.className = className;
+    }
+
+    void display() {
+        System.out.println("Id : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("Class : " + className);
+        System.out.println("Score : " + score);
+    }
+
+    int getScore() {
+//        karena dia get, maka pasti memngembalikan suatu value, yaitu score itu sendiri
+        return score;
+    }
+
+    void setScore(int score) {
+
+        // ini namanya preprocessing, kelebihan setter bisa melakukan preprocessing yaitu kita bisa melakukan something terlebih dahulu
+        if (score > 100 || score < 0){
+            throw new IllegalArgumentException("Score harus 0 - 100");
+        }
+        this.score = score;
+    }
+
+
+    /*"access modifier" biasa digunakan pada encapsulation(melindungi/memprotect)
+    terdapat beberapa modifier :
+
+    tabel aksesnya bisa dimana saja:
+
+                                class   |   package |   turunan |   public
+    1. public                     *     |     *     |      *    |     *
+    2. protected                  *     |     *     |      *    |
+    3. package/default            *     |     *     |           |
+    4. private                    *     |           |           |
+
+    # setter / getter
+    */
+
+    public static void main(String[] args) {
+//      langsung set disini, karena sudah tidak memakai constructor default(constructor tanpa parameter)
+        Student student1 = new Student("samoedra", "PPTI 16");
+//        System.out.println("Score = " + student1.score);
+//        display pakai method
+        student1.display();
+        student1.study();
+        student1.study();
+        student1.setScore(1000);
+        student1.display();
+    }
+}
